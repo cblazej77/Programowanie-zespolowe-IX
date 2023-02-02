7e85e235ae9a78eea9b0f0633b857b806fbfcfe7
RadzkiK <k.domeradzki@outlook.com>
2023-02-02 21:21:17 +0100

    Dodanie interfejsu graficznego dla logowania/rejestracji/powitanie

---
+++ MobileApp/App.js
@@ -0,0 +1,12 @@
+
+//screens
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Welcome from './screens/Welcome';
+
+
+export default function App() {
+  return <Login />;
+}
+
+
---
+++ MobileApp/screens/Login.js
@@ -0,0 +1,117 @@
+import React, {useState} from "react";
+import { StatusBar } from "expo-status-bar";
+
+//formik
+import { Formik } from "formik";
+
+//icons
+import {Octicons, Ionicons, Fontisto} from '@expo/vector-icons'
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledButton,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    ButtonText,
+    MsgBox,
+    Line,
+    Colors,
+    ExtraText,
+    ExtraView,
+    TextLink,
+    TextLinkContent
+} from './../components/styles';
+import { View } from "react-native";
+
+//Colors
+const {brand, darkLight, primary} = Colors;
+
+const Login = () => {
+    const [hidePassword, setHidePassword] = useState(true);
+
+    return (
+        <StyledContainer>
+            <StatusBar style="dark" ></StatusBar>
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('./../assets/img/logo.png')}></PageLogo>
+                <PageTitle>Logowanie</PageTitle>
+                <Formik
+                initialValues={{email: '', password: ''}}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+                >
+                    {({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
+                        <MyTextInput 
+                            label = "Adres Email"
+                            icon="mail"
+                            placeholder="email@example.com"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            keyboardType="email-address"
+                        />
+                        <MyTextInput 
+                            label = "Hasło"
+                            icon="lock"
+                            placeholder="************"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password}
+                            secureTextEntry={hidePassword}
+                            isPassword={true}
+                            hidePassword={hidePassword}
+                            setHidePassword={setHidePassword}
+                        />
+                        <MsgBox>...</MsgBox>
+                        <StyledButton onpress={handleSubmit}>
+                            <ButtonText>
+                                Zaloguj
+                            </ButtonText>
+                        </StyledButton>
+                        <Line />
+                        <StyledButton google={true} onPress={handleSubmit}>
+                            <Fontisto name="google" color={primary} size={25}/>
+                            <ButtonText google={true}>
+                                Zaloguj się z Google
+                            </ButtonText>
+                        </StyledButton>
+                        <ExtraView>
+                                <ExtraText>Nie masz jeszcze konta? </ExtraText>
+                                <TextLink>
+                                    <TextLinkContent>Zarejestruj się!</TextLinkContent>
+                                </TextLink>
+                            </ExtraView>
+                    </StyledFormArea>)}
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    );
+}
+
+const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+            {isPassword && (
+                <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+                    <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
+                </RightIcon>
+            )}
+        </View>
+    )
+}
+
+export default Login;
