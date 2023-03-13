8d61ff53e5f0cfa0adf522844643cf216d2b483e
Jakub Kasinski <jkasinski73@gmail.com>
2023-03-13 21:44:30 +0100

    Added company and artist profile model classes

---
+++ src/main/java/com/pz/login/model/user/Education.java
@@ -0,0 +1,26 @@
+package com.pz.login.model.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Education {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String schoolName;
+    private String faculty;
+    private String fieldOfStudy;
+    private String degree;
+    private String startDate;
+    private String endDate;
+    private String description;
+}
