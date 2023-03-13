057770d29b7f4e02d22bf7e2454db0ccad4fa38a
Jakub Kasinski <jkasinski73@gmail.com>
2023-03-13 17:51:13 +0100

    Added basic global exception handling

---
+++ src/main/java/com/pz/login/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.pz.login.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ErrorObject> handleUsernameNotFoundException(UsernameNotFoundException ex, WebRequest request) {
+        ErrorObject errorObject = new ErrorObject(HttpStatus.UNAUTHORIZED.value(), "Invalid email or password", new Date());
+        return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.UNAUTHORIZED);
+    }
+}
