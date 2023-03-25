3cce41d7baf4b85e0858c027e8622d297695cbf7
Błażej Cieślewicz <102741487+cblazej77@users.noreply.github.com>
2023-03-25 21:02:50 +0100

    Update Gallery.js
    zmiana layoutu danych w galerii

--- MobileApp/components/Gallery.js
+++ MobileApp/components/Gallery.js
@@ -4,18 +4,67 @@ import React from 'react';
 const DATA_images = [
     {
         id: "1",
-        name: "Piotr",
-        surname: "Baran",
-        unseen_messages: 0,
-        last_message_date: Date(2023, 3, 22),
-        last_message: "Ale zajmę się tym."
+        image: "#2b5057"
     },
-    
+    {
+        id: "2",
+        image: "#aad618"
+    },
+    {
+        id: "3",
+        image: "#b79d94"
+    },
+    {
+        id: "4",
+        image: "#5825dc"
+    },
+    {
+        id: "5",
+        image: "#5c7be9"
+    },
+    {
+        id: "6",
+        image: "#4cf8c2"
+    },
+    {
+        id: "7",
+        image: "#8fd075"
+    },
+    {
+        id: "8",
+        image: "#ab6ea6"
+    },
+    {
+        id: "9",
+        image: "#3e18f5"
+    },
+    {
+        id: "10",
+        image: "#f7a9ac"
+    },
+
 ];
 
 const Gallery = ({ data, input, setInput }) => {
     return (
-            <View><Text>Galeria</Text></View>
+        <FlatList
+            style={{ margin: 2 }}
+            numColumns={2}
+            columnWrapperStyle={{ flex: 1, justifyContent: "space-evenly" }}
+            data={DATA_images}
+            keyExtractor={(item, index) => index}
+            ItemSeparatorComponent={<View style={{ width: "100%", height: 2 }} />}
+            renderItem={({ item }) => {
+                return (
+                    <View style={{
+                        backgroundColor: item.image,
+                        width: "49%",
+                        height: 100,
+                    }} />
+                )
+            }}>
+
+        </FlatList>
     )
 }
 
