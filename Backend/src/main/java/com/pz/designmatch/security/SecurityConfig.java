6cb33545deaeae151fb03ea17f366a478a4da06a
Jakub Kasinski <jkasinski73@gmail.com>
2023-03-27 15:29:56 +0200

    Allowed cors

--- Backend/src/main/java/com/pz/designmatch/security/SecurityConfig.java
+++ Backend/src/main/java/com/pz/designmatch/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors().and()
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(authEntryPoint)
