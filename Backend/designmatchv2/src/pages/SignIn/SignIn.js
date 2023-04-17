4b19fcd588eef4ce85e2410b3e51e162455a7973
Błażej Cieślewicz <102741487+cblazej77@users.noreply.github.com>
2023-04-17 17:13:59 +0200

    syntax change

--- Backend/designmatchv2/src/pages/SignIn/SignIn.js
+++ Backend/designmatchv2/src/pages/SignIn/SignIn.js
@@ -41,86 +41,86 @@ export const SignIn = () => {
       setPasswordInvalid(true);
     } else {
       setPasswordInvalid(false);
-
-      e.preventDefault();
-
-      try {
-        const response = await axios.post(LOGIN_URL,
-          JSON.stringify({ email, password }),
-          {
-            headers: { 'Content-Type': 'application/json' },
-          }
-        );
-        console.log(response?.data);
-        console.log(response?.accessToken);
-        console.log(JSON.stringify(response));
-        authApi.login(email, password);
-        navigate(redirectPath, { replace: true });
-      } catch (err) {
-
-        if (!err?.response) {
-          console.log('No Server Response');
-        } else if (err.response?.status === 409) {
-          console.log('Username Taken');
-        } else {
-          console.log('Registration Failed')
-          console.log(err)
-          setPasswordInvalid(true);
-        }
-      }
-      const login = useGoogleLogin({
-        onSuccess: CodeResponse  => {
-          console.log(CodeResponse );
-          authApi.login("Michal", "pssw");
-          navigate(redirectPath, {replace: true});
-        },
-        flow: 'auth-code',
-      });
-/*
-  const buttonEnabled = (username, password) => {
-      if(username.length > 0 && password.length > 2 ) {
-          setEnabled(true);
-      } else {
-          setEnabled(false);
-      }
-  }
-
-  /*
-    const buttonEnabled = (username, password) => {
-        if(username.length > 0 && password.length > 2 ) {
-            setEnabled(true);
-        } else {
-            setEnabled(false);
-        }
     }
-  */
-  return (
-    <AllPage>
-      <MainName >LOGOWANIE</MainName>
-      <StyledForm >
-        <LogoIcon />
-        <Input required type="text" label="email@example.com" id="loginId" onChange={e => setEmail(e.target.value)} />
-        <Input required type="password" label="************" id="passwordId" onChange={e => setPassword(e.target.value)} />
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          {passwordInvalid ? <StyledAlert>Podane hasło jest nieprawidłowe.</StyledAlert> : <></>}
-          <LoginButton to='' type="submit" onClick={e => handleSubmit(e)}>Zaloguj się</LoginButton>
-          {/*{ enabled ?<LoginButton to='/' type="submit" onClick = {e => handleSubmit(e)}>Zaloguj się</LoginButton> :<LoginButton to='' type="submit" onClick= {e => handleSubmit(e)}>Zaloguj się</LoginButton>}*/}
-          {/*<Button to='/sign-up' type="button" >Zarejestruj się</Button>*/}
-          <LineForm />
-          <GoogleButton to='' type="button" onClick={ () => login()} >Kontynuuj z Google{' '}</GoogleButton>
-          <div style={{ fontSize: 12 }}>
-            <text>Nie masz jeszcze konta? </text>
-            <Link to='/sign-up' type="submit">Zarejestruj się!</Link>
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(LOGIN_URL,
+        JSON.stringify({ email, password }),
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+      console.log(response?.data);
+      console.log(response?.accessToken);
+      console.log(JSON.stringify(response));
+      authApi.login(email, password);
+      navigate(redirectPath, { replace: true });
+    } catch (err) {
+
+      if (!err?.response) {
+        console.log('No Server Response');
+      } else if (err.response?.status === 409) {
+        console.log('Username Taken');
+      } else {
+        console.log('Registration Failed')
+        console.log(err)
+        setPasswordInvalid(true);
+      }
+    }
+    const login = useGoogleLogin({
+      onSuccess: CodeResponse => {
+        console.log(CodeResponse);
+        authApi.login("Michal", "pssw");
+        navigate(redirectPath, { replace: true });
+      },
+      flow: 'auth-code',
+    })}
+    /*
+      const buttonEnabled = (username, password) => {
+          if(username.length > 0 && password.length > 2 ) {
+              setEnabled(true);
+          } else {
+              setEnabled(false);
+          }
+      }
+    
+      /*
+        const buttonEnabled = (username, password) => {
+            if(username.length > 0 && password.length > 2 ) {
+                setEnabled(true);
+            } else {
+                setEnabled(false);
+            }
+        }
+      */
+    return (
+      <AllPage>
+        <MainName >LOGOWANIE</MainName>
+        <StyledForm >
+          <LogoIcon />
+          <Input required type="text" label="email@example.com" id="loginId" onChange={e => setEmail(e.target.value)} />
+          <Input required type="password" label="************" id="passwordId" onChange={e => setPassword(e.target.value)} />
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            {passwordInvalid ? <StyledAlert>Podane hasło jest nieprawidłowe.</StyledAlert> : <></>}
+            <LoginButton to='' type="submit" onClick={e => handleSubmit(e)}>Zaloguj się</LoginButton>
+            {/*{ enabled ?<LoginButton to='/' type="submit" onClick = {e => handleSubmit(e)}>Zaloguj się</LoginButton> :<LoginButton to='' type="submit" onClick= {e => handleSubmit(e)}>Zaloguj się</LoginButton>}*/}
+            {/*<Button to='/sign-up' type="button" >Zarejestruj się</Button>*/}
+            <LineForm />
+            <GoogleButton to='' type="button" onClick={() => login()} >Kontynuuj z Google{' '}</GoogleButton>
+            <div style={{ fontSize: 12 }}>
+              <text>Nie masz jeszcze konta? </text>
+              <Link to='/sign-up' type="submit">Zarejestruj się!</Link>
+            </div>
           </div>
-        </div>
 
-      </StyledForm>
-    </AllPage>
-  );
+        </StyledForm>
+      </AllPage>
+    );
 
-  /*
-            <CenterButton>
-              <FacebookButton to='' type="button" >Kontynuuj z Facebook</FacebookButton>
-            </CenterButton>
-  */
-}
+/*
+          <CenterButton>
+            <FacebookButton to='' type="button" >Kontynuuj z Facebook</FacebookButton>
+          </CenterButton>
+*/
+    }
