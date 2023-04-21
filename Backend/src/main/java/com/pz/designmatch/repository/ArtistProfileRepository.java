10b3f6f660bf3f408c7fc3ec5babc63b2a1ec41e
YahorM1 <104639553+YahorM1@users.noreply.github.com>
2023-04-21 20:54:07 +0200

    added artist filter

--- Backend/src/main/java/com/pz/designmatch/repository/ArtistProfileRepository.java
+++ Backend/src/main/java/com/pz/designmatch/repository/ArtistProfileRepository.java
@@ -1,13 +1,17 @@
 package com.pz.designmatch.repository;
 
 import com.pz.designmatch.model.user.ArtistProfile;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ArtistProfileRepository extends JpaRepository<ArtistProfile, Long> {
     Optional<ArtistProfile> findByUser_Username(String username);
     Optional<ArtistProfile> findByUser_Id(Long id);
+
+    List<ArtistProfile> findAll(Specification<ArtistProfile> specification);
 }
