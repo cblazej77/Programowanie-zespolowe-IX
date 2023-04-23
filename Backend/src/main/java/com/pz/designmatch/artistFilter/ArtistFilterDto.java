91d0b4abc9113644067c54d0de63f9c7305a1e5e
YahorM1 <104639553+YahorM1@users.noreply.github.com>
2023-04-23 13:50:00 +0200

    fixed filter error

---
+++ Backend/src/main/java/com/pz/designmatch/artistFilter/ArtistFilterDto.java
@@ -0,0 +1,37 @@
+package com.pz.designmatch.artistFilter;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.pz.designmatch.model.enums.Language;
+import com.pz.designmatch.model.enums.Subcategory;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ArtistFilterDto {
+
+    @JsonProperty("level")
+    private final String level;
+
+    @JsonProperty("location")
+    private final String city;
+
+    @JsonProperty("skills")
+    private final Set<String> skills;
+    @JsonProperty("languages")
+    private final Set<String> languages;
+
+    @JsonProperty("tags")
+    private final Set<String> tags;
+
+    public ArtistFilterDto(String level, String city, Set<String> skills, Set<String> languages, Set<String> tags) {
+        this.level = level;
+        this.city = city;
+        this.skills = skills;
+        this.languages = languages;
+        this.tags = tags;
+    }
+}
--- Backend/src/main/java/com/pz/designmatch/artistFilter/artistFilterController.java
+++ Backend/src/main/java/com/pz/designmatch/artistFilter/artistFilterController.java
@@ -4,8 +4,8 @@ import com.pz.designmatch.model.enums.*;
 import com.pz.designmatch.model.user.ArtistProfile;
 import com.pz.designmatch.repository.ArtistProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,21 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/artist")
 public class artistFilterController {
+    public static final String apiVersionAccept = "application/json";
     @Autowired
     private ArtistProfileRepository artistProfileRepository;
 
-    @GetMapping("/filter")
-    public List<ArtistProfile> filterArtists(@RequestParam(name = "level", required = false) List<Level> level,
-                                             @RequestParam(name = "location", required = false) List<City> city,
-                                             @RequestParam(name = "category", required = false) List<Category> category,
-                                             @RequestParam(name = "language", required = false) List<Language> languages,
-                                             @RequestParam(name = "subcategory", required = false) List<Subcategory> subcategory,
-                                             @RequestParam(name = "tags", required = false) List<Tag> tags) {
+    @GetMapping(value = "/filter", produces = apiVersionAccept)
+    public ResponseEntity<List<ArtistFilterDto>> filterArtists(@RequestParam(name = "level", required = false) List<Level> level,
+                                                               @RequestParam(name = "location", required = false) List<City> city,
+                                                               @RequestParam(name = "category", required = false) List<Category> category,
+                                                               @RequestParam(name = "language", required = false) List<Language> languages,
+                                                               @RequestParam(name = "subcategory", required = false) List<Subcategory> subcategory,
+                                                               @RequestParam(name = "tags", required = false) List<Tag> tags) {
         Specification<ArtistProfile> specification = Specification.where(null);
         if (level != null && !level.isEmpty()) {
             specification = specification.and(ArtistProfileSpecification.hasLevel(level));
@@ -52,10 +54,22 @@ public class artistFilterController {
         if (tags != null && !tags.isEmpty()) {
             specification = specification.and(ArtistProfileSpecification.hasTag(tags));
         }
-        return artistProfileRepository.findAll(specification);
-
+        List<ArtistProfile> artistProfileList = artistProfileRepository.findAll(specification);
+        List<ArtistFilterDto> artistFilterDtos = new ArrayList<>();
+        for (ArtistProfile artistProfile : artistProfileList){
+            Set<String> skillsSet = artistProfile.getSkills().stream().map(Subcategory::toString).collect(Collectors.toSet());
+            Set<String> languagesSet = artistProfile.getLanguages().stream().map(Language::toString).collect(Collectors.toSet());
+            Set<String> tagsSet = artistProfile.getTags().stream().map(Tag::toString).collect(Collectors.toSet());
+            ArtistFilterDto artistFilterDto = new ArtistFilterDto(
+                    artistProfile.getLevel().toString(),
+                    artistProfile.getLocation().toString(),
+                    skillsSet,
+                    languagesSet,
+                    tagsSet
+            );
+            artistFilterDtos.add(artistFilterDto);
+        }
+        return ResponseEntity.ok(artistFilterDtos);
     }
 }
-
-
 
