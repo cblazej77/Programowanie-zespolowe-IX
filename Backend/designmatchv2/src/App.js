2d8cbb281fe73a8cd3583955cba01d1fb129eaa8
Błażej Cieślewicz <102741487+cblazej77@users.noreply.github.com>
2023-05-11 00:51:57 +0200

    Chat.js add
    dodanie strony czatu - Web

--- Backend/designmatchv2/src/App.js
+++ Backend/designmatchv2/src/App.js
@@ -1,40 +1,76 @@
 //Switch => Routes
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { AuthProvider } from './components/Auth';
-import Head from './components/header/Header';
-import Home  from './pages/Home/Home';
-import Account  from './pages/Account';
-import About from './pages/About/About';
-import { SignIn}  from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
-import './App.css';
-import { RequireAuth } from './components/RequireAuth';
-import UserPage from './pages/profile'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import LoadingPage from './pages/LoadingPage';
-import EditUserPage from './pages/Profile/EditUserPage';
-import Modal from './components/ModalA';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./components/Auth";
+import Head from "./components/header/Header";
+import Home from "./pages/Home/Home";
+import Account from "./pages/Account";
+import About from "./pages/About/About";
+import { SignIn } from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import "./App.css";
+import { RequireAuth } from "./components/RequireAuth";
+import UserPage from "./pages/profile";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import LoadingPage from "./pages/LoadingPage";
+import EditUserPage from "./pages/Profile/EditUserPage";
+import Modal from "./components/ModalA";
+import Chat from "./pages/Chat/Chat";
 
 function App() {
-
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <Head />
-      {/* <Alarm /> */}
-      <Routes>
-        <Route path='/'  element={ <Home />} />
-        <Route path='/about' element={ <About />} />
-        <Route path='/account' element={ <RequireAuth> <UserPage /> </RequireAuth> } />
-        <Route path='/sign-in' element={ <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}> <SignIn /> </GoogleOAuthProvider>} />
-        <Route path='/sign-up' element ={ <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}> <SignUp /> </GoogleOAuthProvider>} />
-        <Route path='/accountEdit' element={ <RequireAuth> <EditUserPage /> </RequireAuth> } />
-        <Route path='/test' element ={ <EditUserPage />} />
-        <Route path='/1' element ={ <Modal />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Head />
+        {/* <Alarm /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/account"
+            element={
+              <RequireAuth>
+                {" "}
+                <UserPage />{" "}
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/sign-in"
+            element={
+              <GoogleOAuthProvider
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              >
+                {" "}
+                <SignIn />{" "}
+              </GoogleOAuthProvider>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <GoogleOAuthProvider
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              >
+                {" "}
+                <SignUp />{" "}
+              </GoogleOAuthProvider>
+            }
+          />
+          <Route
+            path="/accountEdit"
+            element={
+              <RequireAuth>
+                {" "}
+                <EditUserPage />{" "}
+              </RequireAuth>
+            }
+          />
+          <Route path="/test" element={<EditUserPage />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/1" element={<Modal />} />
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
