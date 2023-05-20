d2b4457374786ab74f3bc6bfa18b9007cf3e502f
YahorM1 <104639553+YahorM1@users.noreply.github.com>
2023-05-20 22:07:23 +0200

    poprawione wyświetlanie zdjęć

--- Backend/src/main/java/com/pz/designmatch/Images/ImageController.java
+++ Backend/src/main/java/com/pz/designmatch/Images/ImageController.java
@@ -1,25 +1,34 @@
 package com.pz.designmatch.Images;
 
+import com.pz.designmatch.model.user.ArtistProfile;
 import com.pz.designmatch.model.user.PortfolioImages;
 import com.pz.designmatch.repository.ArtistProfileRepository;
 import com.pz.designmatch.repository.PortfolioImagesRepository;
 import com.pz.designmatch.repository.UserRepository;
+import jakarta.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/artist/images")
+@RequestMapping("/public/api/artist/images")
 public class ImageController {
+    private final ServletContext servletContext;
     public static final String apiVersionAccept = "application/json";
     private final ImageService imageService;
     private final ArtistProfileRepository artistProfileRepository;
@@ -27,8 +36,9 @@ public class ImageController {
     private final PortfolioImagesRepository portfolioImagesRepository;
 
     @Autowired
-    public ImageController(ImageService imageService, ArtistProfileRepository artistProfileRepository,
+    public ImageController(ServletContext servletContext, ImageService imageService, ArtistProfileRepository artistProfileRepository,
                            UserRepository userRepository, PortfolioImagesRepository portfolioImagesRepository) {
+        this.servletContext = servletContext;
         this.imageService = imageService;
         this.artistProfileRepository = artistProfileRepository;
         this.userRepository = userRepository;
@@ -40,15 +50,8 @@ public class ImageController {
     @PostMapping(value = "/uploadImages", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = apiVersionAccept)
     public ResponseEntity<String> uploadImageOrBannerProfile(@RequestParam String username, @RequestParam("image") MultipartFile multipartFile,
                                                              @RequestParam(name = "isBanner", defaultValue = "false") boolean isBanner
-    ) throws IOException {
-        //String filename = cleanPath(multipartFile.getOriginalFilename());
-//        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-//        Optional<ArtistProfile> artistProfile = artistProfileRepository.findByUser_Username(username);
-        //artistProfile.setProfileImageUrl(filename);
-        //ArtistProfile savedArtistProfile = artistProfileRepository.save(artistProfile);
+    ) throws IOException, URISyntaxException {
         String imagePath = imageService.uploadProfileImage(username, multipartFile, isBanner);
-        //String uploadDir = "artist-profile/" + savedArtistProfile.getId();
-        //FileUploadUtil.saveFile(uploadDir, filename, multipartFile);
         return ResponseEntity.ok(imagePath);
     }
 
@@ -56,24 +59,35 @@ public class ImageController {
     public ResponseEntity<String> uploadPortfolioImage(@RequestParam String username,
                                                        @RequestParam("image") MultipartFile multipartFile,
                                                        @RequestParam String name,
-                                                       @RequestParam String description) throws IOException {
+                                                       @RequestParam String description) throws IOException, URISyntaxException {
         String imagePath = imageService.uploadPortfolioImage(username, multipartFile, name, description);
-        //String uploadDir = "artist-profile/" + savedArtistProfile.getId();
-        //FileUploadUtil.saveFile(uploadDir, filename, multipartFile);
         return ResponseEntity.ok(imagePath);
     }
 
     @GetMapping(value = "/getProfileImage", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<String> getProfileImage(@RequestParam String username,
+    public ResponseEntity<byte[]> getProfileImage(@RequestParam String username,
                                                   @RequestParam(name = "isBanner", defaultValue = "false") boolean isBanner) throws IOException {
-//        Optional<ArtistProfile> artistProfile = artistProfileRepository.findByUser_Username(username);
-//        ArtistProfile existingProfile = artistProfile.get();
-//        String imagePath = existingProfile.getProfileImageUrl();
-        String imagePath = imageService.getProfileImage(username, isBanner);
-        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-        String imageUrl = baseUrl + imagePath;
-        return ResponseEntity.ok(imageUrl);
+        Optional<ArtistProfile> profileImage = artistProfileRepository.findByUser_Username(username);
+        String imagePath = isBanner ? profileImage.get().getProfileBannerUrl() : profileImage.get().getProfileImageUrl();
+        ClassPathResource imageResource = new ClassPathResource("upload2" + imagePath);
+        InputStream inputStream = imageResource.getInputStream();
+        byte[] imageBytes = StreamUtils.copyToByteArray(inputStream);
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
     }
+
+    @GetMapping(value = "/getPortfolioImage", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getPortfolioImage(@RequestParam Long id) throws IOException {
+        Optional<PortfolioImages> portfolioImages = portfolioImagesRepository.findById(id);
+        String imagePath = portfolioImages.get().getImageUrl();
+        ClassPathResource imageResource = new ClassPathResource("upload2" + imagePath);
+        InputStream inputStream = imageResource.getInputStream();
+        byte[] imageBytes = StreamUtils.copyToByteArray(inputStream);
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
+
+    }
+}
+
+
 //    @GetMapping(value = "/getPortfolioImage")
 //    public ResponseEntity<byte[]> getPortfolioImage(@RequestParam Long id) throws IOException {
 ////        Optional<ArtistProfile> artistProfile = artistProfileRepository.findByUser_Username(username);
@@ -88,19 +102,3 @@ public class ImageController {
 ////        String imageUrl = baseUrl + imagePath;
 ////        return ResponseEntity.ok(imageUrl);
 //    }
-
-    @GetMapping(value = "/getPortfolioImage", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getPortfolioImage(@RequestParam Long id) throws IOException {
-//        Optional<ArtistProfile> artistProfile = artistProfileRepository.findByUser_Username(username);
-//        ArtistProfile existingProfile = artistProfile.get();
-//        String imagePath = existingProfile.getProfileImageUrl();
-        Optional<PortfolioImages> portfolioImages = portfolioImagesRepository.findById(id);
-        String imagePath = portfolioImages.get().getImageUrl();
-        Path path = Paths.get(imagePath);
-        byte[] imageBytes = Files.readAllBytes(path);
-        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
-//        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-//        String imageUrl = baseUrl + imagePath;
-//        return ResponseEntity.ok(imageUrl);
-    }
-}
--- Backend/src/main/java/com/pz/designmatch/Images/ImageService.java
+++ Backend/src/main/java/com/pz/designmatch/Images/ImageService.java
@@ -13,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,20 +30,21 @@ public class ImageService {
 
     @Autowired
     public ImageService(ServletContext servletContext, ArtistProfileRepository artistProfileRepository,
-                        PortfolioImagesRepository portfolioImagesRepository, ExperienceRepository experienceRepository,
-                        EducationRepository educationRepository, UserRepository userRepository) {
+                        PortfolioImagesRepository portfolioImagesRepository) {
         this.servletContext = servletContext;
         this.artistProfileRepository = artistProfileRepository;
         this.portfolioImagesRepository = portfolioImagesRepository;
     }
 
-    private final String UPLOAD_DIR = "C:/Users/User/Desktop/project_Zesp/Programowanie-zespolowe-IX/Backend/uploads";
+    //private final String UPLOAD_DIR = "C:/Users/User/Desktop/project_Zesp/Programowanie-zespolowe-IX/Backend/src/main/resources/upload2/images";
 
     @Transactional
-    public String uploadProfileImage(String username, MultipartFile image, boolean isBannerImage) throws IOException {
-        Path uploadPath = Paths.get(UPLOAD_DIR);
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
+    public String uploadProfileImage(String username, MultipartFile image, boolean isBannerImage) throws IOException, URISyntaxException {
+        URL resourceUrl = getClass().getClassLoader().getResource("upload2/images");
+        assert resourceUrl != null;
+        Path uploadsPath = Paths.get(resourceUrl.toURI());
+        if (!Files.exists(uploadsPath)) {
+            Files.createDirectories(uploadsPath);
         }
         Optional<ArtistProfile> artistProfile = (Optional<ArtistProfile>) Optional.ofNullable(artistProfileRepository.findByUser_Username(username)
                 .orElseThrow(() -> new EntityNotFoundException("Artist profile with username" + username + "not found")));
@@ -59,7 +62,7 @@ public class ImageService {
             throw new IllegalArgumentException("file must be jpg, jpeg or png");
         }
         String newFileName = fileName + "_" + System.currentTimeMillis() + "." + extension;
-        Path destinationPath = uploadPath.resolve(newFileName);
+        Path destinationPath = uploadsPath.resolve(newFileName);
         File dest = destinationPath.toFile();
         try {
             image.transferTo(dest);
@@ -77,21 +80,12 @@ public class ImageService {
     }
 
     @Transactional
-    public String getProfileImage(String username, boolean isBannerImage) {
-        Optional<ArtistProfile> artistProfile = artistProfileRepository.findByUser_Username(username);
-        ArtistProfile existingProfile = artistProfile.get();
-        String imagePath = isBannerImage ? existingProfile.getProfileBannerUrl() : existingProfile.getProfileImageUrl();
-        if (imagePath == null || imagePath.isEmpty()) {
-            throw new EntityNotFoundException("Profile image for artist with username " + username + " not found");
-        }
-        return imagePath;
-    }
-
-    @Transactional
-    public String uploadPortfolioImage(String username, MultipartFile image, String name, String description) throws IOException {
-        Path uploadPath = Paths.get(UPLOAD_DIR);
-        if (!Files.exists(uploadPath)) {
-            Files.createDirectories(uploadPath);
+    public String uploadPortfolioImage(String username, MultipartFile image, String name, String description) throws IOException, URISyntaxException {
+        URL resourceUrl = getClass().getClassLoader().getResource("upload2/images");
+        assert resourceUrl != null;
+        Path uploadsPath = Paths.get(resourceUrl.toURI());
+        if (!Files.exists(uploadsPath)) {
+            Files.createDirectories(uploadsPath);
         }
         Optional<ArtistProfile> artistProfile = Optional.ofNullable(artistProfileRepository.findByUser_Username(username)
                 .orElseThrow(() -> new EntityNotFoundException("Artist profile with username" + username + "not found")));
@@ -106,7 +100,7 @@ public class ImageService {
             throw new IllegalArgumentException("file must be jpg, jpeg or png");
         }
         String newFileName = fileName + "_" + System.currentTimeMillis() + "." + extension;
-        Path destinationPath = uploadPath.resolve(newFileName);
+        Path destinationPath = uploadsPath.resolve(newFileName);
         File dest = destinationPath.toFile();
         try {
             image.transferTo(dest);
@@ -118,20 +112,34 @@ public class ImageService {
         portfolioImagesRepository.save(newPortfolioImage);
         return newImagePath;
     }
-
-    @Transactional
-    public byte[] getPortfolioImage(Long id) throws IOException {
-        Optional<PortfolioImages> portfolioImages = portfolioImagesRepository.findById(id);
-        if (portfolioImages.isEmpty()) {
-            throw new RuntimeException("Profile image for artist with username " + id + " not found");
-        }
-        String imagePath = portfolioImages.get().getImageUrl();
-        String realPath = servletContext.getRealPath(imagePath);
-        Path imagePathObj = Paths.get(realPath);
-
-        return Files.readAllBytes(imagePathObj);
-    }
 }
+
+
+//    @Transactional
+//    public String getProfileImage(String username, boolean isBannerImage) {
+//        Optional<ArtistProfile> artistProfile = artistProfileRepository.findByUser_Username(username);
+//        ArtistProfile existingProfile = artistProfile.get();
+//        String imagePath = isBannerImage ? existingProfile.getProfileBannerUrl() : existingProfile.getProfileImageUrl();
+//        if (imagePath == null || imagePath.isEmpty()) {
+//            throw new EntityNotFoundException("Profile image for artist with username " + username + " not found");
+//        }
+//        return imagePath;
+//    }
+
+//    @Transactional
+//    public byte[] getPortfolioImage(Long id) throws IOException {
+//        Optional<PortfolioImages> portfolioImages = portfolioImagesRepository.findById(id);
+//        if (portfolioImages.isEmpty()) {
+//            throw new RuntimeException("Profile image for artist with username " + id + " not found");
+//        }
+//        String imagePath = portfolioImages.get().getImageUrl();
+//        String realPath = servletContext.getRealPath(imagePath);
+////        String absolutePath = servletContext.getRealPath("/uploads");
+////        File uploadedFile = new File(absolutePath, "your_file_name");
+//        Path imagePathObj = Paths.get(realPath);
+//
+//        return Files.readAllBytes(imagePathObj);
+//    }
 
 
 
